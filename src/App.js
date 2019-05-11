7d998202095d9ca7dc3cd28de979b54864d5f8b4
zahin430 <zhprangon@mun.ca>
2019-05-11 10:26:59 -0230

    Authentication setup finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 
 import LoginForm from './components/LoginForm';
-import { Header } from './components/common';
+import { Header, Button, Card, CardSection, Spinner } from './components/common';
 
 class App extends Component {
-    
+   state = { loggedIn: null };
+
     componentWillMount() {
         // FIREBASE SETUP
         firebase.initializeApp({
@@ -18,15 +19,52 @@ class App extends Component {
             messagingSenderId: "684725270297",
             appId: "1:684725270297:web:d17aae285ea16c03"             
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false});
+            }
+        });
     }
 
+    renderContent() {
+        switch(this.state.loggedIn) {
+            case true:
+                return(
+                    <Card>
+                    <CardSection>
+                    <Button onPress={() => firebase.auth().signOut()}>
+                    Log Out
+                    </Button>
+                    </CardSection>
+                </Card>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return(
+                    <View style = {styles.spinStyle}>
+                         <Spinner size="large"/>
+                    </View>
+                ); 
+               
+        }
+    }
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
+    }
+}
+
+const styles = {
+    spinStyle: {
+        marginTop: '50%'
     }
 }
 
